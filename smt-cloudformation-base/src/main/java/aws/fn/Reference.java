906649964eea7ebaa6ff73867be687dc2e295ccf
Karl Bennett <karl.bennett.smt@gmail.com>
2018-09-30 14:41:21 +1300

    Updated the Reference class toString to work with the fnSub function.

--- smt-cloudformation-base/src/main/java/aws/fn/Reference.java
+++ smt-cloudformation-base/src/main/java/aws/fn/Reference.java
@@ -3,13 +3,15 @@ package aws.fn;
 import aws.StringProperty;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import static java.lang.String.format;
+
 public class Reference extends StringProperty implements Function {
 
     @JsonProperty("Ref")
     private final String name;
 
     public Reference(String name) {
-        super("{ \"Ref\": \"" + name + "\" }");
+        super(format("${%s}", name));
         this.name = name;
     }
 }
